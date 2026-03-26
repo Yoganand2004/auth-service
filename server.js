efe8c774c01b20fbf9f27a11c2568fe7d5382463
Yoganand2004 <yoganand.4k@gmail.com>
2026-03-27 02:04:12 +0530

    Bugs fixed maybe 90 percent

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const cors = require("cors");
 const { createServer } = require('node:http');
 const {Server} = require("socket.io")
 
+app.use(cors())
 const server = createServer(app);
 
 const PORT =8002;
-
+const socketHandler  = require("./src/WebSocket/Socket") 
 const io = new Server(server , {
   cors: {
-    origin: "http://localhost:5173:",
+    origin: "http://localhost:5173",
     credentials: true
   }
 })
+socketHandler(io);
 
 
 server.listen(PORT, () => {
---
+++ src/WebSocket/Socket.js
@@ -0,0 +1,37 @@
+let currentroom = null
+let rooms  ={}
+
+const socketHandler = (io) => {
+
+io.on("connection", (socket) => {
+  console.log("User connected",socket.id);
+
+  socket.on("join-port", (roomId) => {
+    if(currentroom){
+      socket.leave(currentroom)
+    }
+    socket.join(roomId);
+    currentroom=roomId
+    if (!rooms[roomId]) rooms[roomId] = "";
+
+    // send existing text
+    socket.emit("load-text", rooms[roomId]);
+
+  });
+  socket.on("send-text",({roomId,text})=>{
+    rooms[roomId]=text;
+    socket.to(roomId).emit("receive-text",text)
+  })
+  socket.on("leave-port", (portId) => {
+  socket.leave(portId);
+});
+socket.on("clear-room", (portId) => {
+  rooms[portId] = "";                          
+  socket.to(portId).emit("receive-text", ""); 
+});
+
+});
+
+};
+
+module.exports = socketHandler;
