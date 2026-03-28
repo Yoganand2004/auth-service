06b495014d4b400a0158695bbc2f3fb701587bc8
Yoganand2004 <yoganand.4k@gmail.com>
2026-03-29 01:21:28 +0530

    going to deplloy on render

--- server.js
+++ server.js
@@ -6,16 +6,21 @@ const {Server} = require("socket.io")
 app.use(cors())
 const server = createServer(app);
 
-const PORT =8002;
+const PORT = process.env.PORT || 8002;
+
 const socketHandler  = require("./src/WebSocket/Socket") 
 const io = new Server(server , {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
+    methods: ["GET", "POST"],
     credentials: true
   }
 })
 socketHandler(io);
 
+app.get("/", (req, res) => {
+  res.send("WebSocket server running 🚀");
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
